5372f40db4a3df6239180a73ccc54ea7d5c85011
konrad-jaworski <konrad.agh.imir@gmail.com>
2025-10-13 21:59:51 +0200

    Work on physic loss

--- loss_function.py
+++ loss_function.py
@@ -3,20 +3,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class gradCalc:
 
-    def gradient(outputs,inputs):
-        """Computes the partial derivative of an output with respect to an input.
+class GradCalc:
+    """Utility class for computing gradients of outputs w.r.t. inputs."""
+    def __init__(self):
+        pass
+
+    def gradient(self, outputs, inputs):
+        """
+        Computes ∂outputs/∂inputs using autograd.
+
         Args:
             outputs: (N, 1) tensor
-            inputs: (N, D) tensor
+            inputs:  (N, D) tensor, requires_grad=True
+
+        Returns:
+            grad: (N, D) tensor containing partial derivatives.
         """
-        return torch.autograd.grad(
-        outputs, 
-        inputs, 
-        grad_outputs=torch.ones_like(outputs), create_graph=True
-    )
+        grad, = torch.autograd.grad(
+            outputs=outputs,
+            inputs=inputs,
+            grad_outputs=torch.ones_like(outputs),
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True
+        )
+        return grad
+    
+class DiffusiionLoss:
+    def __init__(self):
+        pass
 
-
-
-class physicLoss:
+    def pde_loss(self, model, inputs):
--- networks.py
+++ networks.py
@@ -2,9 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class base_network(nn.Module):
-    def __init__(self, input_size, output_size, hidden_layers, hidden_units, activation_function=F.tanh):
-        super(base_network, self).__init__()
+class BaseNetwork(nn.Module):
+    def __init__(self, input_size, output_size, hidden_layers, hidden_units, activation_function=nn.Tanh()):
+        """
+        Generic fully connected neural network class.
+        """
+        super().__init__()
         self.activation_function = activation_function
         layers = []
         layers.append(nn.Linear(input_size, hidden_units))
@@ -13,9 +16,29 @@ class base_network(nn.Module):
         layers.append(nn.Linear(hidden_units, output_size))
         self.layers = nn.ModuleList(layers)
 
+        for layer in self.layers:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_uniform_(layer.weight)
+                nn.init.zeros_(layer.bias)
+
     def forward(self, x):
         for layer in self.layers[:-1]:
             x = self.activation_function(layer(x))
         x = self.layers[-1](x)
         return x
+    
+class DiffusionNetwork(BaseNetwork):
+    def __init__(self, input_size, output_size, hidden_layers, hidden_units, activation_function=nn.Tanh()):
+        """
+        Anisotropic diffusion network.
+        """
+        super().__init__(input_size, output_size, hidden_layers, hidden_units, activation_function)
+
+        self.a_x = nn.Parameter(torch.randn(1))
+        self.a_y = nn.Parameter(torch.randn(1)) 
+        self.a_z = nn.Parameter(torch.randn(1))
+
+    def forward(self, x):
+        output = super().forward(x)
+        return output
 
